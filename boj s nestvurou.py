aa4f288bf2630900c94b099a14db5a3f9d6a9f9d
TheAnnoyingBard <92982597+TheAnnoyingBard@users.noreply.github.com>
2021-11-05 13:05:57 +0100

    Update boj s nestvurou.py

--- boj s nestvurou.py
+++ boj s nestvurou.py
@@ -2,24 +2,23 @@ from hrac import *
 from nestvura import *
 
 def bojsnestvurou (co, koho):
-    print(koho.profese + " narazil/a na nestvuru. Vypada jako " + co.jmeno +"! Nema na vybranou, jde se bojovat.")
+    print(koho.profese + " narazil*a na nestvuru. Vypada jako " + co.jmeno +"! Nema na vybranou, jde se bojovat.")
     bylsoubojvuli = False
     while co.zivy == True and koho.zivy == True:
-        if koho.pohlavi == "f":
-            if co.silaprotif < 0 and ((koho.vule - 2) - co.vuleprotif)<=(koho.sila - co.silaprotif):
-                if (koho.sila + (hodkostkou())) > (co.silaprotif + (hodkostkou())):
+            if co.sila < 0 and ((koho.vule - 2) - co.vule)<=(koho.sila - co.sila):
+                if (koho.sila + (hodkostkou())) > (co.sila + (hodkostkou())):
                     co.zivoty = co.zivoty -1
                     if co.zivoty == 0:
-                        print(f"{koho.profese} zabila nestvuru známou jako {co.jmeno} a ziskala {co.zkusenost} bodu zkusenoti.")
+                        print(f"{koho.profese} zabil*a nestvuru známou jako {co.jmeno} a ziskal*a {co.zkusenost} bodu zkusenoti.")
                     else:
                         print("Jeden zivot dole, jen tak dal!")
-                elif (koho.sila + (hodkostkou())) == (co.silaprotif + (hodkostkou())):
+                elif (koho.sila + (hodkostkou())) == (co.sila + (hodkostkou())):
                     print("Je to remiza!")
                     break
-                elif (koho.sila + (hodkostkou())) < (co.silaprotif +(hodkostkou())):
+                elif (koho.sila + (hodkostkou())) < (co.sila + (hodkostkou())):
                     print("Nestvura vyhrala. " + koho.profese + "ztraci jeden zivot.")
                     koho.ubranezivoty = koho.ubranezivoty +1
                     if koho.zivoty == 0:
-                        print(self.profese + " precenila sve sily. Budeme na tebe vzpominat.")
+                        print(self.profese + " precenil*a sve sily. Budeme na tebe vzpominat.")
                         koho.zivy = False
                         break
